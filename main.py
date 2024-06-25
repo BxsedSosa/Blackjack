c4be1c8cc73a15687725f998dd0a38c70debd11f
BxsedSosa <donovanxabad@gmail.com>
2024-06-25 10:20:11 -0700

    Got some of the functionality of player and dealer working together

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import sys
+import sys, os
 from random import shuffle
+
+
+def clear_console() -> None:
+    os.system("cls || clear")
 
 
 class Deck:
@@ -33,12 +37,13 @@ class Deck:
 class Hand:
 
     def __init__(self):
-        self.hand = []
+        self.cards = []
         self.score = 0
+        self.under_21 = True
 
     def get_score(self, card):
         if card[1] == "Ace":
-            if self.score + 11 > 21:
+            if self.score + 11 >= 22:
                 self.score += 1
             else:
                 self.score += 11
@@ -47,41 +52,59 @@ class Hand:
         else:
             self.score += int(card[1])
 
-    def draw_card(self, deck):
-        card = deck.deck.pop()
-        self.hand.append(card)
-        self.get_score(card)
+    def check_valid_hand(self):
+        if self.score > 21:
+            return False
 
-
-class Player:
-
-    def __init__(self, hand):
-        self.hand = hand
+        return True
 
 
 class Dealer:
 
-    def __init__(self, hand):
+    def __init__(self, deck, hand, player_hand):
+        self.deck = deck.deck
         self.hand = hand
+        self.player_hand = player_hand
+
+    def start_game(self):
+        for _ in range(2):
+            self.draw_card(self.player_hand)
+            self.draw_card(self.hand)
+
+    def draw_card(self, hand):
+        card = self.deck.pop()
+        hand.cards.append(card)
+        hand.get_score(card)
 
 
 def main():
     deck = Deck()
-    hand = Hand()
-    player = Player()
-    dealer = Dealer()
 
     while True:
-        print(hand.hand)
-        print(hand.score)
+        clear_console()
+        player = Hand()
+        dealer = Dealer(deck, Hand(), player)
+        dealer.start_game()
 
-        user_input = input(f"Enter: \n")
+        while player.under_21:
+            print(f"Score: {player.score}, Cards: {player.cards}")
+            print(f"Score: {dealer.hand.score}, Cards: {dealer.hand.cards[0]}")
+            user_input = input(f"Enter: \n")
+
+            if user_input == "h":
+                dealer.draw_card(player.cards)
+
+            if user_input == "s":
+                break
+
+            clear_console()
+
+            player.under_21 = player.check_valid_hand()
+
+        user_input = input(f"Continue?:\n")
 
         if user_input == "f":
             sys.exit()
-
-        if user_input == "d":
-            hand.draw_card(deck)
 
 
 main()
